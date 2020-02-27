2c17e7d4607f4fb84c3310f44b0dcaad2fcd2fbf
subhamkrazy65 <shubhamk314@gmail.com>
2020-02-27 14:42:40 +0530

    -

---
+++ build/precache-manifest.81642bb30d31524ccdd254db621f778a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6546e7dfc87cdd6fc364",
+    "url": "/static/js/main.6546e7df.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "6546e7dfc87cdd6fc364",
+    "url": "/static/css/main.4666d40a.chunk.css"
+  },
+  {
+    "revision": "26a97026b8f2212e1ec03c4dd1d469fa",
+    "url": "/index.html"
+  }
+];
