3e97a4792bb8a67c41a90ed9105df3f53854ab28
subhamkrazy65 <shubhamk314@gmail.com>
2020-02-29 06:31:30 +0530

    comtents added

---
+++ src/resumeData.js
@@ -0,0 +1,78 @@
+let resumeData = {
+    "imagebaseurl":"https://espektro.in",
+    "name": "Subham Kumar",
+    "role": "Fullstack Developer",
+    "githubId":"subhamkrazy65",
+    "skypeid": "7908171942",
+    "roleDescription": "I am a Full Stack Developer.I am a MERN Stack Developer and Competitive Coder.A self-motivated and hardworking individual constantly involved in self-improvement and always ready to take up challenging tasks to test my skills and improve them further. I love to develop and build things that solve real-world problems In my free time, I read articles and blogs related to programming and new frameworks used in the field of software development. Skilled in React.JS, Node.JS, Data Structures, Backend Development in Express.JS, C++.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/subham-kumar-88593814a/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/subhamkrazy65",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://instagram.com/alak_creatures",
+          "className":"fa fa-instagram"
+        }
+      ],
+    "aboutme":"I am currently a pre-final year student at Kalyani Government Engineering College and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving into Machine Learning. I'm a standup comic which actively participates in various standup comedy events.",
+    "address":"India",
+    "website":"https://espektro.in",
+    "education":[
+      {
+        "UniversityName":"Kalyani Government Engineering College",
+        "specialization":"Fullstack Developer",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2020",
+        "Achievements":""
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Originals",
+        "specialization":"Sales & Marketing Lead",
+        "MonthOfLeaving":"Dec",
+        "YearOfLeaving":"2018",
+        "Achievements":""
+      }
+    ],
+    "skillsDescription":"MERN Stack Developer",
+    "skills":[
+      {
+        "skillname":"Node js"
+      },
+      {
+        "skillname":"React js"
+      },
+      {
+        "skillname":"HTML & CSS"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Espektro",
+        "description":"Webapp",
+        "imgurl":"images/portfolio/espektro.jpeg"
+      },
+      {
+        "name":"Styleo",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/coffee.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"I have a very decent experience working with Subham, we have worked together in various college projects including the Official website of Espektro. He is very good at problem-solving, and ES6 syntaxes. He has good hands-on experience in MERN stack technologies and is a good full stack developer, apart from this, he is a team player and a good human.",
+        "name":"Kiran Kumar Das"
+      }
+    ]
+  }
+  
+  export default resumeData
